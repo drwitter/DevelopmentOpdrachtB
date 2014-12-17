c7326e8b2e54ad38e27961c06e25989fba35e140
drwitter <davey.r.witter@gmail.com>
2014-12-17 14:09:02 +0100

    v1.3

--- src/test/java/TestMain.java
+++ src/test/java/TestMain.java
@@ -7,7 +7,7 @@ public class TestMain {
     @Test
     public void testPasses() {
         int expected = 33;
-        int output = Main.reken(14,19);
+        int output = Main.reken(14,9);
         assertEquals(output, expected);
     }
 
